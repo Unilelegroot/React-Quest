eed61f039be813bd7f1b29b757cc0cc72c6dabd1
Unilelegroot <unilelegroot@gmail.com>
2026-08-14 20:07:41 -0300

    feat: inicio mission-card

---
+++ src/components/MissionCard.jsx
@@ -0,0 +1,22 @@
+import "./missionCard.css" 
+
+function MissionCard({ title, description, technology, difficulty, xp, completed, onToggle}) {
+    return(
+        <article className={`mission-card ${completed ? "mission-card-complemented" : ""}`}> 
+            <div className="mission-card_header">
+                <span className="mission-card_technology">{technology}</span>
+                <span className="mission-card_difficulty">{difficulty}</span>
+            </div>
+            <h3>{title}</h3>
+            <p className="mission-card_description">{description}</p>
+            <div className="mission-card_footer">
+                <strong className="mission-card_xp">{xp} XP</strong>
+                <button type="button" className="mission-card_button" onClick={onToggle}>
+                    {completed ? "Reabrir missão" : "Concluir missão"}
+                </button>
+            </div>
+        </article>
+    )
+}
+
+export default MissionCard
